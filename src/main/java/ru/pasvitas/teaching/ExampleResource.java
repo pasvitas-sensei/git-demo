2ff9c5610b8eefec179d98fb86c85886bfb7da9a
Vitaly Pashichev <pasvitas1@gmail.com>
2024-03-16 12:50:41 +0300

    My new commit

--- src/main/java/ru/pasvitas/teaching/ExampleResource.java
+++ src/main/java/ru/pasvitas/teaching/ExampleResource.java
@@ -1,6 +1,7 @@
 package ru.pasvitas.teaching;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -13,4 +14,7 @@ public class ExampleResource {
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
+
+    @POST
+    public String newHello() { return "new Hello"; }
 }
